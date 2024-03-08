4eec26f79c6d885e9585012e0d5bcf151d3b8e23
jonwhoo <107388668+jonwhoo@users.noreply.github.com>
2024-03-08 19:00:47 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,10 +15,10 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git packages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
-echo 'src-git luci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
-echo 'src-git routing https://github.com/coolsnowwolf/routing' >>feeds.conf.default
-echo 'src-git telephony https://git.openwrt.org/feed/telephony.git' >>feeds.conf.default
+#echo 'src-git packages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
+#echo 'src-git luci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
+#echo 'src-git routing https://github.com/coolsnowwolf/routing' >>feeds.conf.default
+#echo 'src-git telephony https://git.openwrt.org/feed/telephony.git' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
